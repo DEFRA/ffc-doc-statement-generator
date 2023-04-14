744c7b9561c523b71fe3dea824ea263808921031
Marc Templeton <marcte@kainos.com>
2023-04-14 15:14:09 +0100

    app insights test fixed

--- test/unit/insights.test.js
+++ test/unit/insights.test.js
@@ -27,14 +27,19 @@ describe('Application Insights', () => {
 
   test('does not setup application insights if no instrumentation key', () => {
     const appInsights = require('../../app/insights')
+    delete process.env.APPINSIGHTS_INSTRUMENTATIONKEY
+
     appInsights.setup()
+
     expect(applicationInsights.setup.mock.calls.length).toBe(0)
   })
 
   test('does setup application insights if instrumentation key present', () => {
     const appInsights = require('../../app/insights')
     process.env.APPINSIGHTS_INSTRUMENTATIONKEY = 'test-key'
+
     appInsights.setup()
+
     expect(applicationInsights.setup.mock.calls.length).toBe(1)
   })
 })
