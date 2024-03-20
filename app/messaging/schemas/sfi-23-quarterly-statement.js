3c19840ac15c2de7f0009a15d7b7a067a1b6ab74
Leigh Godson <139965284+GodsonLeigh@users.noreply.github.com>
2024-03-20 11:43:42 +0000

    adding sfi-3-quarterly-statement schema

---
+++ app/messaging/schemas/sfi-23-quarterly-statement.js
@@ -0,0 +1,23 @@
+const Joi = require('joi')
+
+const businessName = require('./business-name')
+const frn = require('./frn')
+const sbi = require('./sbi')
+const email = require('./email')
+const address = require('./address')
+const funding = require('./funding')
+const payment = require('./payment')
+const scheme = require('./scheme')
+const documentReference = require('./document-reference')
+
+module.exports = Joi.object({
+  businessName,
+  frn,
+  sbi,
+  email,
+  address,
+  scheme,
+  documentReference,
+  payments: Joi.array().items(payment).required().min(1),
+  funding: Joi.array().items(funding).required().min(2).has(funding.keys({ name: 'Total' }).required())
+}).required()
