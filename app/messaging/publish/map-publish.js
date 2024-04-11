b010b1644f4385f86324783b6280394a1bf6a8ce
Leigh Godson <139965284+GodsonLeigh@users.noreply.github.com>
2024-04-11 10:28:46 +0100

    changed publish.js and increased test coverage

--- app/messaging/publish/map-publish.js
+++ app/messaging/publish/map-publish.js
@@ -1,21 +1,25 @@
 const MESSAGE_SOURCE = require('../../constants/message-source')
 
 const mapPublish = (document, filename, type) => {
+  const body = {
+    businessName: document.businessName,
+    sbi: document.sbi,
+    frn: document.frn,
+    address: document.address,
+    email: document.email,
+    filename,
+    scheme: document.scheme,
+    documentReference: document?.documentReference ?? null
+  }
+
+  if (document.paymentPeriod) {
+    body.paymentPeriod = document.paymentPeriod
+  }
+
   return {
-    body: {
-      businessName: document.businessName,
-      sbi: document.sbi,
-      frn: document.frn,
-      address: document.address,
-      email: document.email,
-      filename,
-      scheme: document.scheme,
-      documentReference: document?.documentReference ?? null,
-      paymentPeriod: document?.paymentPeriod ?? ''
-    },
+    body,
     type: `uk.gov.doc.${type}.publish`,
     source: MESSAGE_SOURCE
   }
 }
-
 module.exports = mapPublish
--- test/mocks/messages/publish.js
+++ test/mocks/messages/publish.js
@@ -1,9 +1,10 @@
 const MESSAGE_SOURCE = require('../../../app/constants/message-source')
-const { STATEMENT: STATEMENT_TYPE, SCHEDULE: SCHEDULE_TYPE } = require('../../../app/constants/document-types')
+const { STATEMENT: STATEMENT_TYPE, SCHEDULE: SCHEDULE_TYPE, SFI23QUARTERLYSTATEMENT: SFI23QUARTERLYSTATEMENT_TYPE } = require('../../../app/constants/document-types')
 
-const { STATEMENT: STATEMENT_FILENAME, SCHEDULE: SCHEDULE_FILENAME } = require('../components/filename')
+const { STATEMENT: STATEMENT_FILENAME, SCHEDULE: SCHEDULE_FILENAME, SFI23QUARTERLYSTATEMENT: SFI23QUARTERLYSTATEMENT_FILENAME } = require('../components/filename')
 
 const STATEMENT = require('../mock-statement')
+const SFI23QUARTERLYSTATEMENT = require('../mock-statement-sfi23-quarterly')
 const { topUpSchedule: SCHEDULE } = require('../mock-schedule')
 
 const BASE_MESSAGE = {
@@ -27,6 +28,22 @@ const STATEMENT_MESSAGE = {
   type: `uk.gov.doc.${STATEMENT_TYPE.id}.publish`
 }
 
+const SFI23QUARTERLYSTATEMENT_MESSAGE = {
+  ...BASE_MESSAGE,
+  body: {
+    businessName: SFI23QUARTERLYSTATEMENT.businessName,
+    sbi: SFI23QUARTERLYSTATEMENT.sbi,
+    frn: SFI23QUARTERLYSTATEMENT.frn,
+    address: SFI23QUARTERLYSTATEMENT.address,
+    email: SFI23QUARTERLYSTATEMENT.email,
+    filename: SFI23QUARTERLYSTATEMENT_FILENAME,
+    scheme: SFI23QUARTERLYSTATEMENT.scheme,
+    documentReference: SFI23QUARTERLYSTATEMENT?.documentReference ?? null,
+    paymentPeriod: SFI23QUARTERLYSTATEMENT?.paymentPeriod ?? ''
+  },
+  type: `uk.gov.doc.${SFI23QUARTERLYSTATEMENT_TYPE.id}.publish`
+}
+
 const SCHEDULE_MESSAGE = {
   ...BASE_MESSAGE,
   body: {
@@ -44,5 +61,6 @@ const SCHEDULE_MESSAGE = {
 
 module.exports = {
   STATEMENT_MESSAGE,
+  SFI23QUARTERLYSTATEMENT_MESSAGE,
   SCHEDULE_MESSAGE
 }
