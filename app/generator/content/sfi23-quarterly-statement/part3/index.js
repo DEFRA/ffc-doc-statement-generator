8976d9dae7c1dc4d57ee0e32ca50954dd41fd1ad
Abidemi Adio <abidemi.adio@defra.gov.uk>
2024-04-18 09:52:02 +0100

    Fixed unit test

--- app/generator/content/sfi23-quarterly-statement/part3/index.js
+++ app/generator/content/sfi23-quarterly-statement/part3/index.js
@@ -10,31 +10,32 @@ const part3 = (sfi23QuarterlyStatement) => {
 
   return {
     stack: [
-      [{stack: [{ text: 'How your payment was calculated', style: 'header2' },
-      { text: 'We calculated your total payment by adding together:' },
-      {
-        ul: [{
-          text: [
-            'the quarterly payments for your actions '
-          ]
-        },
-        {
-          text: [
-            'any additional payments '
-          ]
-        }],
-        listStyle: 'square',
-        margin: [15, 2, 10, 20]
-      },
-      {
-        text: [
-          'You can check which of your land parcels are part of each action in your SFI 2023 agreement. Find your SFI 2023 agreement by logging into the Rural Payments service at ',
-          { text: 'https://www.ruralpayments.service.gov.uk/customer-account/login', link: 'https://www.ruralpayments.service.gov.uk/customer-account/login', decoration: 'underline' }
-        ]
-      }],
-      unbreakable: true
-    }
-    ],
+      [{
+        stack: [{ text: 'How your payment was calculated', style: 'header2' },
+          { text: 'We calculated your total payment by adding together:' },
+          {
+            ul: [{
+              text: [
+                'the quarterly payments for your actions '
+              ]
+            },
+            {
+              text: [
+                'any additional payments '
+              ]
+            }],
+            listStyle: 'square',
+            margin: [15, 2, 10, 20]
+          },
+          {
+            text: [
+              'You can check which of your land parcels are part of each action in your SFI 2023 agreement. Find your SFI 2023 agreement by logging into the Rural Payments service at ',
+              { text: 'https://www.ruralpayments.service.gov.uk/customer-account/login', link: 'https://www.ruralpayments.service.gov.uk/customer-account/login', decoration: 'underline' }
+            ]
+          }],
+        unbreakable: true
+      }
+      ],
       getActionsTable(nonAdditionalPaymentActionGroup, nonAdditionalPaymentGroupName, true, sfi23QuarterlyStatement.totalActionPayments),
       getActionsTable(additionalPaymentActionGroup, additionalPaymentGroupName, false, sfi23QuarterlyStatement.totalAdditionalPayments),
       getTotalPaymentTable(sfi23QuarterlyStatement.totalPayments)
