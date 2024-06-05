f71797d95bdb673a14b45779bae0cc47f1bb461e
AbidemiAdio <98023713+AbidemiAdio@users.noreply.github.com>
2024-06-05 14:23:57 +0100

    DOC-1051-Hide Action, Management and Total tables, and total mismatch… (#28)
    * DOC-1051-Hide Action, Management and Total tables, and total mismatch explanatory statement
    
    * Implemented task requirements
    
    * Added missing msg block IRO Test FB
    
    * Added missing msg block IRO Test FB
    
    * Added missing msg block IRO Test FB
    
    * Added missing msg block IRO Test FB

--- app/generator/content/sfi23-quarterly-statement/part3/index.js
+++ app/generator/content/sfi23-quarterly-statement/part3/index.js
@@ -1,42 +1,60 @@
 const getActionsTable = require('./get-actions-table')
 const getTotalPaymentTable = require('./get-total-payment-table')
+const { SFI23QUARTERLYSTATEMENT } = require('../../../../constants/document-types')
 
 const part3 = (sfi23QuarterlyStatement) => {
+  const margin2 = 2
+  const margin10 = 10
+  const margin15 = 15
+  const margin20 = 20
+  const ruralPaymentLoginUrl = 'https://www.ruralpayments.service.gov.uk/customer-account/login'
+  const paymentCalculationMessage = {
+    stack: [{ text: 'How your payment was calculated', style: 'header2' },
+      { text: 'We calculated your total payment by adding together:' },
+      {
+        ul: [{
+          text: [
+            'the quarterly payments for your actions '
+          ]
+        },
+        {
+          text: [
+            'any additional payments '
+          ]
+        }],
+        listStyle: 'square',
+        margin: [margin15, margin2, margin10, margin20]
+      },
+      {
+        text: [
+          'You can check which of your land parcels are part of each action in your SFI 2023 agreement. Find your SFI 2023 agreement by logging into the Rural Payments service at ',
+          { text: ruralPaymentLoginUrl, link: ruralPaymentLoginUrl, decoration: 'underline' }
+        ]
+      }]
+  }
+
+  if (SFI23QUARTERLYSTATEMENT.showCalculation === false) {
+    paymentCalculationMessage.stack.push('\n')
+    return {
+      stack: [
+        [{
+          stack: paymentCalculationMessage.stack,
+          unbreakable: true
+        }
+        ]
+      ]
+    }
+  }
   const actions = sfi23QuarterlyStatement.actionGroups
   const nonAdditionalPaymentGroupName = 'Actions'
   const additionalPaymentGroupName = 'Additional payment'
   const nonAdditionalPaymentActionGroup = actions.filter(val => val.groupName !== additionalPaymentGroupName)
   const additionalPaymentActionGroup = actions.filter(val => val.groupName === additionalPaymentGroupName)
-  const margin2 = 2
-  const margin10 = 10
-  const margin15 = 15
-  const margin20 = 20
 
   return {
     stack: [
       [{
-        stack: [{ text: 'How your payment was calculated', style: 'header2' },
-          { text: 'We calculated your total payment by adding together:' },
-          {
-            ul: [{
-              text: [
-                'the quarterly payments for your actions '
-              ]
-            },
-            {
-              text: [
-                'any additional payments '
-              ]
-            }],
-            listStyle: 'square',
-            margin: [margin15, margin2, margin10, margin20]
-          },
-          {
-            text: [
-              'You can check which of your land parcels are part of each action in your SFI 2023 agreement. Find your SFI 2023 agreement by logging into the Rural Payments service at ',
-              { text: 'https://www.ruralpayments.service.gov.uk/customer-account/login', link: 'https://www.ruralpayments.service.gov.uk/customer-account/login', decoration: 'underline' }
-            ]
-          }],
+        stack: paymentCalculationMessage.stack,
         unbreakable: true
       }
       ],
--- test/unit/sfi-23-quarterly-statement/part3/index.test.js
+++ test/unit/sfi-23-quarterly-statement/part3/index.test.js
@@ -1,6 +1,7 @@
 const getActionsTable = require('../../../../app/generator/content/sfi23-quarterly-statement/part3/get-actions-table')
 const getTotalPaymentTable = require('../../../../app/generator/content/sfi23-quarterly-statement/part3/get-total-payment-table')
 const part3 = require('../../../../app/generator/content/sfi23-quarterly-statement/part3')
+const { SFI23QUARTERLYSTATEMENT } = require('../../../../app/constants/document-types')
 
 jest.mock('../../../../app/generator/content/sfi23-quarterly-statement/part3/get-actions-table', () => jest.fn())
 jest.mock('../../../../app/generator/content/sfi23-quarterly-statement/part3/get-total-payment-table', () => jest.fn())
@@ -11,7 +12,8 @@ describe('part3', () => {
     getTotalPaymentTable.mockClear()
   })
 
-  test('should correctly generate the part3 object', () => {
+  test('should correctly generate the part3 object when showCalculation is true', () => {
+    SFI23QUARTERLYSTATEMENT.showCalculation = true
     const sfi23QuarterlyStatement = {
       actionGroups: [
         { groupName: 'Actions' },
@@ -65,6 +67,61 @@ describe('part3', () => {
         'mockedActionsTable1',
         'mockedActionsTable2',
         'mockedTotalPaymentTable'
+      ]
+    })
+  })
+
+  test('should not generate calculation tables when showCalculation is false', () => {
+    SFI23QUARTERLYSTATEMENT.showCalculation = false
+    const sfi23QuarterlyStatement = {
+      actionGroups: [
+        { groupName: 'Actions' },
+        { groupName: 'Additional payment' },
+        { groupName: 'Actions' }
+      ],
+      totalActionPayments: 1000,
+      totalAdditionalPayments: 500,
+      totalPayments: 1500
+    }
+
+    getActionsTable.mockReturnValueOnce('mockedActionsTable1')
+    getActionsTable.mockReturnValueOnce('mockedActionsTable2')
+    getTotalPaymentTable.mockReturnValue('mockedTotalPaymentTable')
+
+    const result = part3(sfi23QuarterlyStatement)
+
+    expect(getActionsTable).not.toHaveBeenCalled()
+    expect(getTotalPaymentTable).not.toHaveBeenCalled()
+
+    expect(result).toEqual({
+      stack: [
+        [{
+          stack: [{ text: 'How your payment was calculated', style: 'header2' },
+            { text: 'We calculated your total payment by adding together:' },
+            {
+              ul: [{
+                text: [
+                  'the quarterly payments for your actions '
+                ]
+              },
+              {
+                text: [
+                  'any additional payments '
+                ]
+              }],
+              listStyle: 'square',
+              margin: [15, 2, 10, 20]
+            },
+            {
+              text: [
+                'You can check which of your land parcels are part of each action in your SFI 2023 agreement. Find your SFI 2023 agreement by logging into the Rural Payments service at ',
+                { text: 'https://www.ruralpayments.service.gov.uk/customer-account/login', link: 'https://www.ruralpayments.service.gov.uk/customer-account/login', decoration: 'underline' }
+              ]
+            },
+            '\n'],
+          unbreakable: true
+        }
+        ]
       ]
     })
   })
