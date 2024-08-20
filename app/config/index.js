4ba21be58fa620253cfa532b2ab5bc2833aaf4e6
Sam Plackett <60177449+samplackett@users.noreply.github.com>
2024-08-20 10:10:22 +0100

    change behaviour of env variables (#35)

--- app/config/index.js
+++ app/config/index.js
@@ -23,10 +23,10 @@ const config = {
   statementReceiverEndpoint: process.env.STATEMENT_RECEIVER_ENDPOINT,
   schedulesArePublished: process.env.SCHEDULES_ARE_PUBLISHED,
   showSfi23PaymentPeriod: process.env.SHOW_SFI_23_PAYMENT_PERIOD,
-  sfi23QuarterlyStatementEnabled: process.env.SFI23QUARTERLYSTATEMENT_ENABLED,
-  scheduleEnabled: process.env.SCHEDULE_ENABLED,
-  sendCrmMessageEnabled: process.env.SEND_CRM_MESSAGE_ENABLED,
-  saveLogEnabled: process.env.SAVE_LOG_ENABLED
+  sfi23QuarterlyStatementEnabled: typeof process.env.SFI23QUARTERLYSTATEMENT_ENABLED === 'boolean' ? process.env.SFI23QUARTERLYSTATEMENT_ENABLED : false,
+  scheduleEnabled: typeof process.env.SCHEDULE_ENABLED === 'boolean' ? process.env.SCHEDULE_ENABLED : false,
+  sendCrmMessageEnabled: typeof process.env.SEND_CRM_MESSAGE_ENABLED === 'boolean' ? process.env.SEND_CRM_MESSAGE_ENABLED : false,
+  saveLogEnabled: typeof process.env.SAVE_LOG_ENABLED === 'boolean' ? process.env.SAVE_LOG_ENABLED : false
 }
 
 const result = schema.validate(config, {
