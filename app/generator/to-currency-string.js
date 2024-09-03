ed975ff1976db2d9b50154cf41e242304dbe88b9
Charlie Benger-Stevenson <suityou01@yahoo.co.uk>
2024-09-03 15:25:19 +0100

    SFI-23 statement to use 2dps for totals with quotients

--- app/generator/to-currency-string.js
+++ app/generator/to-currency-string.js
@@ -1,8 +1,11 @@
 const toCurrencyString = (value) => {
-  const numParts = value.split('.')
+  if (isNaN(value)) throw new Error("Currency value is not a number")
+  let currencyString = value.toString()
+  if (!currencyString.includes('.')) return currencyString.replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+  const numParts = currencyString.split('.')
   numParts[0] = numParts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',')
   numParts[1] = numParts[1] ? numParts[1].padEnd(2, '0') : '00'
-  return `£${numParts.join('.')}`
+  return `${numParts.join('.')}`
 }
 
 module.exports = toCurrencyString
