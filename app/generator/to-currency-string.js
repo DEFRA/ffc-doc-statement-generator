7782c1dea945a424f2c668746631769f3e2df6e7
Charlie Benger-Stevenson <suityou01@yahoo.co.uk>
2024-09-04 11:47:37 +0100

    Fixed Linting error

--- app/generator/to-currency-string.js
+++ app/generator/to-currency-string.js
@@ -1,6 +1,6 @@
 const toCurrencyString = (value) => {
-  if (isNaN(value)) throw new Error("Currency value is not a number")
-  let currencyString = value.toString()
+  if (isNaN(value)) throw new Error('Currency value is not a number')
+  const currencyString = value.toString()
   if (!currencyString.includes('.')) return currencyString.replace(/\B(?=(\d{3})+(?!\d))/g, ',')
   const numParts = currencyString.split('.')
   numParts[0] = numParts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',')
