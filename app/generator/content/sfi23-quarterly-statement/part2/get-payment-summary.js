2f799ee35e2c1a28e9a5454979a21c3db05c10aa
Charlie Benger-Stevenson <suityou01@yahoo.co.uk>
2024-09-04 16:09:23 +0100

    Unit test fixes

--- app/generator/content/sfi23-quarterly-statement/part2/get-payment-summary.js
+++ app/generator/content/sfi23-quarterly-statement/part2/get-payment-summary.js
@@ -9,7 +9,7 @@ const getPaymentSummary = (sfi23QuarterlyStatement) => {
     paymentSummary.push({ text: [{ text: 'Period: ', bold: true, lineBreak: false }, `${sfi23QuarterlyStatement.paymentPeriod}`] })
   }
 
-  paymentSummary.push({ text: [{ text: 'Payment: ', bold: true, lineBreak: false }, `£${toCurrencyString(sfi23QuarterlyStatement.paymentAmount).toString()}`] })
+  paymentSummary.push({ text: [{ text: 'Payment: ', bold: true, lineBreak: false }, `${toCurrencyString(sfi23QuarterlyStatement.paymentAmount).toString()}`] })
   paymentSummary.push({ text: `This is usually paid into your account within 2 working days of ${moment(sfi23QuarterlyStatement.transactionDate).format('LL')}.` })
   paymentSummary.push({ text: [{ text: 'Payment reference: ', bold: true, lineBreak: false }, `${sfi23QuarterlyStatement.paymentReference}`] })
 
--- app/generator/to-currency-string.js
+++ app/generator/to-currency-string.js
@@ -1,11 +1,11 @@
 const toCurrencyString = (value) => {
   if (isNaN(value)) throw new Error('Currency value is not a number')
   const currencyString = value.toString()
-  if (!currencyString.includes('.')) return currencyString.replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+  if (!currencyString.includes('.')) return `£${currencyString.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`
   const numParts = currencyString.split('.')
   numParts[0] = numParts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',')
   numParts[1] = numParts[1] ? numParts[1].padEnd(2, '0') : '00'
-  return `${numParts.join('.')}`
+  return `£${numParts.join('.')}`
 }
 
 module.exports = toCurrencyString
