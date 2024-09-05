6c58e6a0b9275f1a7ff933bdd9f7be6c1d543ffd
Charlie Benger-Stevenson <suityou01@yahoo.co.uk>
2024-09-05 09:50:49 +0100

    Added curly brackets to if statements

--- app/generator/to-currency-string.js
+++ app/generator/to-currency-string.js
@@ -1,7 +1,11 @@
 const toCurrencyString = (value) => {
-  if (isNaN(value)) throw new Error('Currency value is not a number')
+  if (isNaN(value)) {
+    throw new Error('Currency value is not a number')
+  }
   const currencyString = value.toString()
-  if (!currencyString.includes('.')) return `£${currencyString.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`
+  if (!currencyString.includes('.')) {
+    return `£${currencyString.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`
+  }
   const numParts = currencyString.split('.')
   numParts[0] = numParts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',')
   numParts[1] = numParts[1] ? numParts[1].padEnd(2, '0') : '00'
