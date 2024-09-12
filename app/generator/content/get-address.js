a48060ca9aadbdee4262e404b3a4fe4b4c464787
Leigh Godson <139965284+GodsonLeigh@users.noreply.github.com>
2024-09-12 11:44:26 +0100

    fix a bug in positioning

--- app/generator/content/get-address.js
+++ app/generator/content/get-address.js
@@ -1,17 +1,14 @@
-const { millimetresToPoints } = require('../conversion')
 const MAX_CHARACTERS_PER_LINE = 50
 
 const getAddress = (businessName, address) => {
   let fullAddress = `${businessName.toUpperCase().substring(0, MAX_CHARACTERS_PER_LINE)}\n`
   const addressLines = Object.values(address)
-  const topMargin = 45
-  const leftMargin = 0
   addressLines.filter(x => x !== undefined && x !== null && x !== '').forEach(x => {
     fullAddress += `${x.toUpperCase().substring(0, MAX_CHARACTERS_PER_LINE)}\n`
   })
   return {
     stack: [
-      { text: fullAddress, style: 'address', absolutePosition: { x: millimetresToPoints(leftMargin), y: millimetresToPoints(topMargin) } }
+      { text: fullAddress, style: 'address' }
     ]
   }
 }
