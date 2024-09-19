5ec710c1b8cc453e8dfa17a940056b4818970f58
Leigh Godson <139965284+GodsonLeigh@users.noreply.github.com>
2024-09-19 19:42:46 +0100

    fixed address to 7 lines and removed gaps between blank lines for missing values

--- app/generator/content/get-address.js
+++ app/generator/content/get-address.js
@@ -3,9 +3,18 @@ const MAX_CHARACTERS_PER_LINE = 50
 const getAddress = (businessName, address) => {
   let fullAddress = `${businessName.toUpperCase().substring(0, MAX_CHARACTERS_PER_LINE)}\n`
   const addressLines = Object.values(address)
-  addressLines.filter(x => x !== undefined && x !== null && x !== '').forEach(x => {
+
+  const sanitizedAddressLines = addressLines.filter(line => line !== null && line !== undefined && line !== '')
+
+  // Ensure there are always 7 lines
+  while (sanitizedAddressLines.length < 7) {
+    sanitizedAddressLines.push('')
+  }
+
+  sanitizedAddressLines.forEach((x) => {
     fullAddress += `${x.toUpperCase().substring(0, MAX_CHARACTERS_PER_LINE)}\n`
   })
+
   return {
     stack: [
       { text: fullAddress, style: 'address' }
--- test/unit/generator/content/get-address.test.js
+++ test/unit/generator/content/get-address.test.js
@@ -13,7 +13,7 @@ describe('get address', () => {
 
     test('should return business name in uppercase and new line', () => {
       const result = getAddress(businessName, address)
-      expect(result.stack[0].text).toBe(`${businessName.toUpperCase()}\n`)
+      expect(result.stack[0].text).toBe(`${businessName.toUpperCase()}\n\n\n\n\n\n\n\n`)
     })
 
     test('should return style as address', () => {
@@ -29,7 +29,7 @@ describe('get address', () => {
 
     test('should return business name and each address value in uppercase and truncated to 50 characters, separated by new lines', () => {
       const result = getAddress(businessName, address)
-      const expectedAddress = `${businessName.toUpperCase()}\n${address.line1.toUpperCase().substring(0, 50)}\n${address.line2.toUpperCase().substring(0, 50)}\n${address.line3.toUpperCase().substring(0, 50)}\n${address.line4.toUpperCase().substring(0, 50)}\n${address.line5.toUpperCase().substring(0, 50)}\n${address.postcode.toUpperCase().substring(0, 50)}\n`
+      const expectedAddress = `${businessName.toUpperCase()}\n${address.line1.toUpperCase().substring(0, 50)}\n${address.line2.toUpperCase().substring(0, 50)}\n${address.line3.toUpperCase().substring(0, 50)}\n${address.line4.toUpperCase().substring(0, 50)}\n${address.line5.toUpperCase().substring(0, 50)}\n${address.postcode.toUpperCase().substring(0, 50)}\n\n`
       expect(result.stack[0].text).toBe(expectedAddress)
     })
 
@@ -51,7 +51,7 @@ describe('get address', () => {
 
     test('should return business name and each address value in uppercase and truncated to 50 characters, separated by new lines ignoring those which are undefined', () => {
       const result = getAddress(businessName, address)
-      const expectedAddress = `${businessName.toUpperCase()}\n${address.line2.toUpperCase().substring(0, 50)}\n${address.line3.toUpperCase().substring(0, 50)}\n${address.line5.toUpperCase().substring(0, 50)}\n`
+      const expectedAddress = `${businessName.toUpperCase()}\n${address.line2.toUpperCase().substring(0, 50)}\n${address.line3.toUpperCase().substring(0, 50)}\n${address.line5.toUpperCase().substring(0, 50)}\n\n\n\n\n`
       expect(result.stack[0].text).toBe(expectedAddress)
     })
 
@@ -73,7 +73,7 @@ describe('get address', () => {
 
     test('should return business name and each address value in uppercase and truncated to 50 characters, separated by new lines ignoring those which are null', () => {
       const result = getAddress(businessName, address)
-      const expectedAddress = `${businessName.toUpperCase()}\n${address.line2.toUpperCase().substring(0, 50)}\n${address.line3.toUpperCase().substring(0, 50)}\n${address.line5.toUpperCase().substring(0, 50)}\n`
+      const expectedAddress = `${businessName.toUpperCase()}\n${address.line2.toUpperCase().substring(0, 50)}\n${address.line3.toUpperCase().substring(0, 50)}\n${address.line5.toUpperCase().substring(0, 50)}\n\n\n\n\n`
       expect(result.stack[0].text).toBe(expectedAddress)
     })
 
@@ -95,7 +95,7 @@ describe('get address', () => {
 
     test('should return business name and each address value in uppercase and truncated to 50 characters, separated by new lines ignoring those which are empty', () => {
       const result = getAddress(businessName, address)
-      const expectedAddress = `${businessName.toUpperCase()}\n${address.line2.toUpperCase().substring(0, 50)}\n${address.line3.toUpperCase().substring(0, 50)}\n${address.line5.toUpperCase().substring(0, 50)}\n`
+      const expectedAddress = `${businessName.toUpperCase()}\n${address.line2.toUpperCase().substring(0, 50)}\n${address.line3.toUpperCase().substring(0, 50)}\n${address.line5.toUpperCase().substring(0, 50)}\n\n\n\n\n`
       expect(result.stack[0].text).toBe(expectedAddress)
     })
 
