8ae74c44cfc286fc85e5e54093d57be26525e9cd
Leigh Godson <139965284+GodsonLeigh@users.noreply.github.com>
2024-09-19 19:44:48 +0100

    magic number change for address lines

--- app/generator/content/get-address.js
+++ app/generator/content/get-address.js
@@ -1,13 +1,13 @@
 const MAX_CHARACTERS_PER_LINE = 50
-
+const addressLineAmount = 7
 const getAddress = (businessName, address) => {
   let fullAddress = `${businessName.toUpperCase().substring(0, MAX_CHARACTERS_PER_LINE)}\n`
   const addressLines = Object.values(address)
 
   const sanitizedAddressLines = addressLines.filter(line => line !== null && line !== undefined && line !== '')
 
-  // Ensure there are always 7 lines
-  while (sanitizedAddressLines.length < 7) {
+  // !!important - Ensure there are always 7 lines
+  while (sanitizedAddressLines.length < addressLineAmount) {
     sanitizedAddressLines.push('')
   }
 
