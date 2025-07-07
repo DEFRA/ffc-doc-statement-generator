0ec865b5b07e49c525562ea38bdb0d86e6256076
Leigh Godson <leigh.godson@atos.net>
2025-07-07 15:51:28 +0100

    changed paymentAmountCalculated to paymentAmount, updated line spacing

--- app/generator/content/delinked-statement/payment-summary/get-payment-summary.js
+++ app/generator/content/delinked-statement/payment-summary/get-payment-summary.js
@@ -3,10 +3,10 @@ const toCurrencyString = require('../../../to-currency-string')
 
 const getPaymentSummary = (delinkedStatement) => {
   moment.locale('en-gb')
-  const paymentSummary = [{ text: 'What you\'ve been paid \n', style: 'tableHeader2' }]
-  paymentSummary.push({ text: [{ text: 'Payment amount: ', bold: true, lineBreak: false, style: 'separator' }, `${toCurrencyString(delinkedStatement.paymentAmount).toString()}`] })
-  paymentSummary.push({ text: `This is usually paid into your account within 2 working days of ${moment(delinkedStatement.transactionDate).format('LL')}.` })
-  paymentSummary.push({ text: [{ text: 'Payment reference: ', bold: true, lineBreak: false }, `${delinkedStatement.paymentReference}`] })
+  const paymentSummary = [{ text: '\n What you\'ve been paid \n', style: 'tableHeader2' }]
+  paymentSummary.push({ text: [{ text: 'Payment amount: ', bold: true, lineBreak: false, style: 'separator' }, `${toCurrencyString(delinkedStatement.paymentAmount).toString()}`], margin: [0, 10, 0, 5] })
+  paymentSummary.push({ text: `This is usually paid into your account within 2 working days of ${moment(delinkedStatement.transactionDate).format('LL')}.`, margin: [0, 5, 0, 5] })
+  paymentSummary.push({ text: [{ text: 'Payment reference: ', bold: true, lineBreak: false }, `${delinkedStatement.paymentReference}`], margin: [0, 5, 0, 5] })
 
   return paymentSummary
 }
