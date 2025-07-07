39902c7867690154f350b5452be71eaf36efb540
Leigh Godson <leigh.godson@atos.net>
2025-07-07 17:06:12 +0100

    updating margin fine detail

--- app/generator/content/delinked-statement/payment-summary/get-payment-summary.js
+++ app/generator/content/delinked-statement/payment-summary/get-payment-summary.js
@@ -2,12 +2,12 @@ const moment = require('moment')
 const toCurrencyString = require('../../../to-currency-string')
 const zeroMargin = 0
 const fiveMargin = 5
-const tenMargin = 10
+const fifteenMargin = 15
 
 const getPaymentSummary = (delinkedStatement) => {
   moment.locale('en-gb')
-  const paymentSummary = [{ text: '\n What you\'ve been paid \n', style: 'tableHeader2' }]
-  paymentSummary.push({ text: [{ text: 'Payment amount: ', bold: true, lineBreak: false, style: 'separator' }, `${toCurrencyString(delinkedStatement.paymentAmount).toString()}`], margin: [zeroMargin, tenMargin, zeroMargin, fiveMargin] })
+  const paymentSummary = [{ text: 'What you\'ve been paid', style: 'tableHeader2', margin: [zeroMargin, fiveMargin, zeroMargin, zeroMargin] }]
+  paymentSummary.push({ text: [{ text: 'Payment amount: ', bold: true, lineBreak: false, style: 'separator' }, `${toCurrencyString(delinkedStatement.paymentAmount).toString()}`], margin: [zeroMargin, fifteenMargin, zeroMargin, fiveMargin] })
   paymentSummary.push({ text: `This is usually paid into your account within 2 working days of ${moment(delinkedStatement.transactionDate).format('LL')}.`, margin: [zeroMargin, fiveMargin, zeroMargin, fiveMargin] })
   paymentSummary.push({ text: [{ text: 'Payment reference: ', bold: true, lineBreak: false }, `${delinkedStatement.paymentReference}`], margin: [zeroMargin, fiveMargin, zeroMargin, fiveMargin] })
 
