cad9b52550a35530d7fe61d8354a3a5c21443980
Sam Plackett <samuel.plackett@eviden.com>
2025-09-01 09:17:20 +0100

    very well sonar

--- app/utility/common-schema-fields.js
+++ app/utility/common-schema-fields.js
@@ -48,12 +48,14 @@ const stringSchema = (field, max, pattern) => {
     schema = schema.pattern(pattern).messages({
       'string.pattern.base': `${field} is not in the correct format`
     })
+    return schema
   } else if (max !== undefined) {
     schema = schema.max(max).messages({
       'string.max': messages.stringMax(field, max)
     })
+  } else {
+    return schema
   }
-  return schema
 }
 
 const emailSchema = (field, max) => {
