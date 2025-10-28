47eb7230bf64d033e70900b9901af051a4be1316
John Barnard <john.barnard.external@eviden.com>
2025-10-28 13:27:41 +0000

    Save our new fields when creating generation record
    Signed-off-by: John Barnard <john.barnard.external@eviden.com>

--- app/generator/save-log.js
+++ app/generator/save-log.js
@@ -1,13 +1,44 @@
 const db = require('../data')
 
 const saveLog = async (statementData, filename, dateGenerated) => {
-  const { documentReference: documentRef, ...data } = statementData
+  const {
+    documentReference: documentRef,
+    BusinessName,
+    Frn,
+    Sbi,
+    AddressLine1,
+    AddressLine2,
+    AddressLine3,
+    AddressLine4,
+    AddressLine5,
+    Postcode,
+    Email,
+    SchemeName,
+    SchemeShortName,
+    SchemeYear,
+    SchemeFrequency,
+    ...rest
+  } = statementData
 
   return db.generation.create({
-    statementData: data,
+    statementData: rest,
     documentReference: documentRef,
     filename,
-    dateGenerated
+    dateGenerated,
+    businessName: BusinessName,
+    frn: Frn,
+    sbi: Sbi,
+    addressLine1: AddressLine1,
+    addressLine2: AddressLine2,
+    addressLine3: AddressLine3,
+    addressLine4: AddressLine4,
+    addressLine5: AddressLine5,
+    postcode: Postcode,
+    email: Email,
+    schemeName: SchemeName,
+    schemeShortName: SchemeShortName,
+    schemeYear: SchemeYear,
+    schemeFrequency: SchemeFrequency
   })
 }
 
