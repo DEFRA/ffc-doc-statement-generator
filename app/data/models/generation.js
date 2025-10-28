51783dd3922b383424f9290cc1c776df4c9b0011
John Barnard <john.barnard.external@eviden.com>
2025-10-28 13:17:57 +0000

    Added few columns to generations table that will be extracted from statementData
    Signed-off-by: John Barnard <john.barnard.external@eviden.com>

--- app/data/models/generation.js
+++ app/data/models/generation.js
@@ -4,7 +4,21 @@ module.exports = (sequelize, DataTypes) => {
     statementData: DataTypes.JSON,
     dateGenerated: DataTypes.DATE,
     filename: DataTypes.STRING,
-    documentReference: DataTypes.INTEGER
+    documentReference: DataTypes.INTEGER,
+    businessName: DataTypes.STRING,
+    frn: DataTypes.BIGINT,
+    sbi: DataTypes.INTEGER,
+    addressLine1: DataTypes.STRING,
+    addressLine2: DataTypes.STRING,
+    addressLine3: DataTypes.STRING,
+    addressLine4: DataTypes.STRING,
+    addressLine5: DataTypes.STRING,
+    postcode: DataTypes.STRING,
+    email: DataTypes.STRING,
+    schemeName: DataTypes.STRING,
+    schemeShortName: DataTypes.STRING,
+    schemeYear: DataTypes.STRING,
+    schemeFrequency: DataTypes.STRING
   },
   {
     tableName: 'generations',
