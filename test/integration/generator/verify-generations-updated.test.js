167e8dee02694c511c673e14525606147c338c75
John Barnard <john.barnard.external@eviden.com>
2025-10-30 11:19:34 +0000

    Corrected statementData structure in tests to match reality
    Signed-off-by: John Barnard <john.barnard.external@eviden.com>

--- test/integration/generator/verify-generations-updated.test.js
+++ test/integration/generator/verify-generations-updated.test.js
@@ -34,19 +34,23 @@ describe('Liquibase migration populates columns from statementData', () => {
     expect(inserted.sbi).toBe(parseInt(inserted.statementData.sbi))
   })
 
-  test('should populate address fields from statementData', () => {
-    expect(inserted.addressLine1).toBe(inserted.statementData.addressLine1)
+  test('should populate address fields from statementData.address', () => {
+    expect(inserted.addressLine1).toBe(inserted.statementData.address.line1)
+    expect(inserted.addressLine2).toBe(inserted.statementData.address.line2)
+    expect(inserted.addressLine3).toBe(inserted.statementData.address.line3)
+    expect(inserted.addressLine4).toBe(inserted.statementData.address.line4)
+    expect(inserted.addressLine5).toBe(inserted.statementData.address.line5)
+    expect(inserted.postcode).toBe(inserted.statementData.address.postcode)
   })
 
-  test('should populate scheme fields from statementData', () => {
-    expect(inserted.schemeName).toBe(inserted.statementData.schemeName)
-    expect(inserted.schemeShortName).toBe(inserted.statementData.schemeShortName)
-    expect(inserted.schemeYear).toBe(inserted.statementData.schemeYear)
-    expect(inserted.schemeFrequency).toBe(inserted.statementData.schemeFrequency)
+  test('should populate scheme fields from statementData.scheme', () => {
+    expect(inserted.schemeName).toBe(inserted.statementData.scheme.name)
+    expect(inserted.schemeShortName).toBe(inserted.statementData.scheme.shortName)
+    expect(inserted.schemeYear).toBe(inserted.statementData.scheme.year)
+    expect(inserted.schemeFrequency).toBe(inserted.statementData.scheme.frequency)
   })
 
-  test('should populate email and postcode from statementData', () => {
+  test('should populate email from statementData', () => {
     expect(inserted.email).toBe(inserted.statementData.email)
-    expect(inserted.postcode).toBe(inserted.statementData.postcode)
   })
 })
