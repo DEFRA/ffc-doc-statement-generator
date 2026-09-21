656d48b3622cc0a3f56bc1f7c844162b821fa646
Oliver Lewington <oliver.a.lewington@gmail.com>
2026-09-21 15:04:56 +0100

    Simplifying the table declarations

--- app/data/index.js
+++ app/data/index.js
@@ -1,14 +1,9 @@
 const config = require('../config')
 const { Database } = require('ffc-database')
+const TABLES = require('../constants/tables')
 
 const dbConfig = config.dbConfig[config.env]
 
-const tables = {
-  generations: 'generations',
-  noNotifys: 'noNotifys',
-  outbox: 'outbox'
-}
-
-const database = new Database({ ...dbConfig, tables })
+const database = new Database({ ...dbConfig, tables: TABLES })
 
 module.exports = database.connect()
